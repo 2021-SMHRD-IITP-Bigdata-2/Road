b470b9a77220333b1b86b8b9bb3439e85c318c74
ChanI <aok458@gmail.com>
2021-11-05 16:15:02 +0900

    mbti

--- server/routes/map.js
+++ server/routes/map.js
@@ -4,6 +4,7 @@ const {response} = require("express");
 router.use(express.static('public'));
 const db = require('./connection');
 const {route} = require("express/lib/router");
+const {RowDataPacket} = require("mysql/lib/protocol/packets");
 router.use(express.json());
 
 router.get('/', function(req, res, next) {
@@ -14,12 +15,16 @@ router.post("/food",(req,res)=>{
     console.log(req.body.food)
     const foodName = "%"+req.body.food+"%"
     // res.json(req.body.food)
-    const sqlSelect = "SELECT * FROM foods where food_name LIKE ?"
+    const sqlSelect = "SELECT * FROM foods f, restaurants r where food_name LIKE ? and f.food_seq = r.food_seq";
     db.query(sqlSelect,foodName,(err,foods)=>{
         console.log(foods)
         res.json(foods)
-        res.render('map', {foods : foods})
-    })
-})
+
+        // var json = foods;
+        // Object.keys(json).forEach(function(k){
+        //     console.log('키값 : '+k + ', 데이터값 : ' + json[k]);
+        // });
+    });
+});
 
 module.exports = router;
