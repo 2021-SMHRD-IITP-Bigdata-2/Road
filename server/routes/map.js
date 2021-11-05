def7dfaa114c0cbe72151d71b22267ec9f9dcc75
ChanI <aok458@gmail.com>
2021-11-05 11:47:17 +0900

    값은 가져와졌다 그다음은???

--- server/routes/map.js
+++ server/routes/map.js
@@ -4,9 +4,22 @@ const {response} = require("express");
 router.use(express.static('public'));
 const db = require('./connection');
 const {route} = require("express/lib/router");
+router.use(express.json());
 
 router.get('/', function(req, res, next) {
     res.render('map.ejs')
 });
+
+router.post("/food",(req,res)=>{
+    console.log(req.body.food)
+    const foodName = "%"+req.body.food+"%"
+    // res.json(req.body.food)
+    const sqlSelect = "SELECT * FROM foods where food_name LIKE ?"
+    db.query(sqlSelect,foodName,(err,foods)=>{
+        console.log(foods)
+        res.json(foods)
+        res.render('map', {foods : foods})
+    })
+})
 
 module.exports = router;
